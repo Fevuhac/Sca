e3d06e7d160cba35ac407963bdbe7d338aed5e57
hoang-nguyen-vn <hoangnhonline@gmail.com>
2019-01-25 13:34:09 +0700

    fix

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -319,6 +319,15 @@
     <span class="txt">HỖ TRỢ NHANH</span>
     <span class="img"><img src="https://cdn.worldvectorlogo.com/logos/facebook-messenger-white.svg" alt="Facebook Chat"></span>
 </a> -->
+<!--Start of Zendesk Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="https://v2.zopim.com/?6KW934DgOCuVshxZLUBoTFBDFNbRS6EV";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
+<!--End of Zendesk Chat Script-->
 <style>
 .fb-chat {
     position: fixed;
